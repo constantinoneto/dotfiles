91a2bdfefabdeffd997b915ec92678e81755b9b5
Constantino Augusto Dias Neto <constantinoneto@gmail.com>
2012-07-13 11:43:32 -0300

    first aliases

---
+++ lib/aliases.bash
@@ -0,0 +1,12 @@
+
+alias vp='cd /home/projetos/rails && vim +NERDTree'
+alias v='vim'
+alias df='df -h'
+alias which="type -path"
+alias where="type -all"
+alias ll="ls -l"
+alias l="ls -l"
+alias la="ls -la"
+alias rm="rm -i"
+alias mv="mv -i"
+alias cp="cp -iv --preserve=timestamps"
